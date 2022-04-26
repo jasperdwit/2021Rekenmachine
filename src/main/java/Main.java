d7725bc0024d1018e9fc76e532eb9d36c59aab7c
fugo38 <furkan_gocer@hotmail.com>
2022-04-26 12:20:56 +0200

    multiplycomputation gemaakt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         JavaFXApp.main (args);
+
+    }
+}
+
+interface Icomputation{
+    public int compute(int getal1, int getal2);
+
+}
+
+class MultiplyComputation implements Icomputation{
+
+    @Override
+    public int compute(int getal1, int getal2) {
+        return (getal1 * getal2);
     }
 }
